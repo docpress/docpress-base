6a96aae48bb1a4dcbd48a6a405e1356cf0eff1b0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-16 01:43:46 +0800

    Refactors the way styles are tracked

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ const assign = Object.assign
 const buildJs = require('./lib/build_js')
 const buildCss = require('./lib/build_css')
 const hash = require('./lib/hash')
+const eachCons = require('./lib/helpers/each_cons')
 
 /**
  * Metalsmith middleware
  */
 
 module.exports = function base (options) {
+  const ctx = {
+    styles: [],
+    scripts: []
+  }
+
   var app = ware()
-    .use(addJs)
-    .use(addCss)
-    .use(relayout)
+    .use(addJs.bind(ctx))
+    .use(addCss.bind(ctx))
+    .use(addExternalCss.bind(ctx))
+    .use(relayout.bind(ctx))
 
   return function (files, ms, done) {
     app.run(files, ms, done)
@@ -32,28 +39,18 @@ module.exports = function base (options) {
 function addCss (files, ms, done) {
   if (files['assets/style.css']) return done()
 
-  function getAssetFile (file) {
-    return files[file] &&
-      ms.path(ms.source(), ms.metadata().docs, file)
-  }
-
-  buildCss({
-    before:
-      getAssetFile('assets/variables.scss') ||
-      getAssetFile('assets/variables.sass'),
-    after:
-      getAssetFile('assets/custom.scss') ||
-      getAssetFile('assets/custom.sass')
-  }, (err, contents) => {
+  // TODO: stylus import
+  buildCss({}, (err, contents) => {
     if (err) return done(err)
-    delete files['assets/variables.scss']
-    delete files['assets/variables.sass']
-    delete files['assets/custom.scss']
-    delete files['assets/custom.sass']
     files['assets/style.css'] = { contents }
+    this.styles.push('assets/style.css?t=' + hash(contents))
     done()
   })
 }
+
+/**
+ * Add JavaScript
+ */
 
 function addJs (files, ms, done) {
   buildJs((err, contents) => {
@@ -64,8 +61,22 @@ function addJs (files, ms, done) {
       files['assets/script.js'].contents = contents + '\n' +
         files['assets/script.js'].contents
     }
+    this.scripts.push('assets/script.js?t=' +
+      hash(files['assets/script.js'].contents))
     done()
   })
+}
+
+/**
+ * Add external CSS
+ */
+
+function addExternalCss (files, ms, done) {
+  const meta = ms.metadata()
+  const externalCss = getCss(meta)
+  // TODO: account for local styles
+  this.styles = this.styles.concat(externalCss)
+  done()
 }
 
 /**
@@ -97,19 +108,13 @@ function relayout (files, ms, done) {
   const path = fs.readFileSync(join(__dirname, 'data/layout.jade'), 'utf-8')
   const layout = jade.compile(path, { pretty: true })
   const meta = ms.metadata()
-  const externalCss = getCss(meta)
-  const hashes = {
-    style: hash(files['assets/style.css'].contents),
-    script: hash(files['assets/script.js'].contents)
-  }
 
   eachCons(index, (_, fname, __, prevName, ___, nextName) => {
     if (!fname.match(/\.html$/)) return
     const file = files[fname]
     const base = Array(fname.split('/').length).join('../')
-    const styles = [ `${base}assets/style.css?t=${hashes.style}` ]
-      .concat(externalCss)
-    const scripts = [ `${base}assets/script.js?t=${hashes.script}` ]
+    const styles = this.styles.map(relativize(base))
+    const scripts = this.scripts.map(relativize(base))
 
     file.contents = layout(assign({}, file, {
       base, toc, index, meta, styles, scripts,
@@ -122,23 +127,20 @@ function relayout (files, ms, done) {
   done()
 }
 
+function relativize (base) {
+  return function (url) {
+    if (url.substr(0, 1) === '/' || url.match(/^https?:\/\//)) {
+      return url
+    }
+
+    return base + url
+  }
+}
+
 function getCss (meta) {
   let css = meta.css
   if (!css) return []
 
   if (typeof css === 'string') css = [css]
-  return css.map((c) => {
-    return c // TODO: add hashes if it's local
-  })
-}
-
-function eachCons (list, fn) {
-  var keys = Object.keys(list)
-  keys.forEach((key, idx) => {
-    const prevKey = keys[idx - 1]
-    const nextKey = keys[idx + 1]
-    fn(list[key], key,
-       prevKey && list[prevKey], prevKey,
-       nextKey && list[nextKey], nextKey)
-  })
+  return css // TODO: add hashes if it's local
 }
